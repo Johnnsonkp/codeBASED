62d2d534de131515547c5e6faf9e27d3474895b0
John Nkp <john.nkp1@gmail.com>
2024-12-18 20:40:36 +1100

    minor styling tweaks

--- src/components/Languages/LanguageNav.jsx
+++ src/components/Languages/LanguageNav.jsx
@@ -8,7 +8,7 @@ function LanguageNav({languageOptions, style, setLanguage, language }) {
     <div 
       style={{
         // border: '0.1px solid #606266', 
-        width: '40px',
+        width: '30px',
         borderRadius: '5px',
         height: '100%',
         maxHeight: '785px',
@@ -19,28 +19,28 @@ function LanguageNav({languageOptions, style, setLanguage, language }) {
       }}
     >
       {languageOptions.map((lang, index) => (
-          <button 
-            onClick={() => setLanguage(languageOptions.filter((option) => option.value == lang.value))}
-            key={index} 
-            style={{
-              marginTop: '7px', 
-              boxSizing: 'border-box', 
-              width: '40px', 
-              marginRight: 'auto',
-              marginLeft: 'auto',
-              display: 'flex', 
-              justifyContent: 'center',
-              background: `${language[0].value == lang.value? "#51FA7B" : "transparent" }`,
-              // border: '0.1px solid #606266',
-              border: `1px solid ${theme == 'light'? '#EBEBEB' : '#3C3C3C'}`,
+        <button 
+          onClick={() => setLanguage(languageOptions.filter((option) => option.value == lang.value))}
+          key={index} 
+          style={{
+            marginTop: '7px', 
+            boxSizing: 'border-box', 
+            width: '30px', 
+            marginRight: 'auto',
+            marginLeft: 'auto',
+            display: 'flex', 
+            justifyContent: 'center',
+            background: `${language[0].value == lang.value? "#51FA7B" : "transparent" }`,
+            // border: '0.1px solid #606266',
+            border: `1px solid ${theme == 'light'? '#EBEBEB' : '#3C3C3C'}`,
 
-            }}
-          >
-            {lang && lang.image? 
-              <img style={{width: '25px', padding: '0px'}} src={lang.image} /> : 
-              lang.name
-            } 
-          </button>
+          }}
+        >
+          {lang && lang.image? 
+            <img style={{width: '20px', padding: '0px'}} src={lang.image} /> : 
+            lang.name
+          } 
+        </button>
         ))
       }
     </div>
