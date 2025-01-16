41f360a8415ff1ba309225b0c42e82e0209fea4c
John Nkp <john.nkp1@gmail.com>
2025-01-17 06:22:13 +1100

    updated server env var

--- src/components/auth/GitHuboAuth.jsx
+++ src/components/auth/GitHuboAuth.jsx
@@ -17,6 +17,7 @@ const GitHubOAuth = ({setAuthorized, setUserInformation}) => {
     const redirectURI = GITHUB_CALLBACK_URL
     window.location.href = `https://github.com/login/oauth/authorize?response_type=code&redirect_uri=${redirectURI}&scope=user:email&client_id=${clientID}`
   };
+  
   const handleLogin = async (code) => {
     fetch(`${server_URL}/auth/github/callback`, {
       method: 'POST',
