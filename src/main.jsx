98a27097f4e1dbe0f03f3e81bf3c88fd049544bf
John Nkp <john.nkp1@gmail.com>
2025-03-01 18:58:10 +1100

    test deployment

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router';
 import React, {Suspense, lazy} from 'react';
 
+import Auth from './components/auth/Auth.jsx';
 import ChallengeProvider from './store/challengeProvider.jsx';
-// import GithubOAuth from './components/auth/GithubOAuth';
 import LoadingOverlay from './components/Common/Loading/Loading.jsx';
 import Nav from './components/Nav/Nav.jsx';
 import ReactDOM from 'react-dom/client';
@@ -13,8 +13,11 @@ import { ThemeProvider } from './components/theme-provider.tsx';
 import TopBanner from './components/Nav/TopBanner.jsx';
 import UserProvider from './store/userProvider.jsx';
 
+// import GithubOAuth from './components/auth/GithubOAuth';
+
+
 const root = document.getElementById("root");
-const GithubOAuth = lazy(() => import('./components/auth/GithubOAuth'))
+// const GithubOAuth = lazy(() => import('./components/auth/GithubOAuth'))
 const App = lazy(() => import('./App.jsx'))
 
 ReactDOM.createRoot(root).render(
@@ -27,9 +30,9 @@ ReactDOM.createRoot(root).render(
           <BrowserRouter>
           <Suspense fallback={<LoadingOverlay />}>
             <Routes>
-              <Route path="/" element={<GithubOAuth/>} /> 
+              <Route path="/" element={<Auth/>} /> 
                 <Route path="/dashboard" element={<App />} />
-                {/* <Route path="/auth/github/callback" element={<GithubOAuth/>} />  */}
+                <Route path="/auth/github/callback" element={<Auth/>} /> 
               </Routes>
             </Suspense>
           </BrowserRouter>
