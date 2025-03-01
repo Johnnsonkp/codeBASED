6a6f744769d6ddf27a5c18e0a6dede48a5dd52bf
John Nkp <john.nkp1@gmail.com>
2025-03-01 19:08:51 +1100

    test deployment

--- src/components/auth/Auth.jsx
+++ src/components/auth/Auth.jsx
@@ -1,10 +1,61 @@
-import GithubOAuth from './GithuboAuth';
-import React from 'react'
+import React, { useContext, useEffect } from 'react';
+import { fetchUserInfo, handleLoginWithCode } from '../../api/userService.js';
+import { storeUserData, userLoginError } from '../../store/actions/userActions';
+
+import AuthTemplatePage from './AuthTemplatePage.jsx';
+import { UserContext } from '../../store/context/UserContext';
+import { getCodeFromURL } from '../../helpers/auth/GithubAuth.js';
+import { useNavigate } from 'react-router';
 
 export default function Auth() {
+  const GITHUB_CLIENT_ID = import.meta.env.VITE_APP_GITHUB_CLIENT_ID;
+  const githubOAuthURL = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&scope=user`;
+  
+  const {userState, userDispatch} = useContext(UserContext)
+  const isUserAuth = userState.authorised
+  const code = getCodeFromURL();
+  const navigate = useNavigate()
+
+  const loginWithGitHub = () => {
+    window.location.href = encodeURI(githubOAuthURL);
+  };
+
+  const handleData = (data) => {
+    if (data && data?.status && data?.status !== 200) {
+      userLoginError(userDispatch, data.status)
+    } else if (data && isUserAuth == false){
+      storeUserData(data, userDispatch)
+      navigate('/dashboard');
+    }
+  }
+  
+  const handleLoadDemoUserData = async () => {
+    try {
+      const data = await fetchUserInfo();
+      handleData(data);
+    } catch (error) {
+      console.error("An error occurred while loading demo user data:", error);
+      userLoginError(userDispatch, "An error occurred while loading user data ")
+      navigate('/');
+    }
+  };
+
+  const handleLogin = async (code) => {
+    const data = await handleLoginWithCode(code);
+    handleData(data);
+  }
+  
+  useEffect(() => {
+    if (code && isUserAuth == false) {
+      handleLogin(code);
+    }
+  }, [code])
+
   return (
-    <div >
-      <GithubOAuth />
-    </div>
-  )
-}
+    <AuthTemplatePage 
+      githubAuth={loginWithGitHub}
+      loadDemoUser={handleLoadDemoUserData}
+    />
+  );
+};
+
